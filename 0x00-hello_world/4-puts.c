5f12c9068e7a0595f6f1c6f2ddbd794e4dde85ad
Lenaice <lawrencelena8@gmail.com>
2022-06-30 18:03:48 -0700

    corrected 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-/**
- * main - this is a program that prints a message
- * Return: returns 0 if message prints successfully
- */
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art, \n");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
