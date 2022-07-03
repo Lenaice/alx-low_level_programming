e70f727171c51d006c7f32705388b8d693049a76
Lenaice <lawrencelena8@gmail.com>
2022-07-03 15:10:03 -0700

    6 corrected

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,8 +12,8 @@ int main(void)
 	char charType;
 	// sizeof evaluates the size of a variable
 	printf("Size of int: %zu bytes\n", sizeof(intType));
-	printf("Size of float: %zu bytes\n", sizeof(floatType))
-	printf("Size of double: %zu bytes\n", sizeof(doubleType))
-	printf("Size of char: %zu bytes\n", sizeof(charType))
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
+	printf("Size of double: %zu bytes\n", sizeof(doubleType));
+	printf("Size of char: %zu bytes\n", sizeof(charType));
 	return (0);
 }
