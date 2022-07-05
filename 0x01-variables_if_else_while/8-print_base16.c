cf46efa0be2f243aa83c5a27294a61750d77c065
Lenaice <lawrencelena8@gmail.com>
2022-07-04 19:45:58 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * return - always 0
+ */
+int main(void)
+
+{
+	char ch;
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10);
+	return (10);
+}
