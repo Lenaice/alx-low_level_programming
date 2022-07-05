0b4ec843be5c815b297248cc39912b31f32e350d
Lenaice <lawrencelena8@gmail.com>
2022-07-04 18:08:53 -0700

    4-print_alphabt.c correct

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main -> Entry point
  *
  * Return - Always 0
  */
 int main(void)
 {
 	char ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		if (ch != 'q' && ch != 'e')
